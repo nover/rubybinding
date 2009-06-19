8f67de28667a1ba5b084b9b72d35f9dfcce1f243
levi <levi@apollo>
2009-06-18 22:09:08 -0400

    Stub in text editor extension.

---
+++ Gui/RubyTextEditorExtension.cs
@@ -0,0 +1,62 @@
+//
+// RubyTextEditorExtension.cs
+//
+// Authors:
+//  Levi Bard <taktaktaktaktaktaktaktaktaktak@gmail.com> 
+//
+// Copyright (C) 2009 Levi Bard
+//
+// This source code is licenced under The MIT License:
+//
+// Permission is hereby granted, free of charge, to any person obtaining
+// a copy of this software and associated documentation files (the
+// "Software"), to deal in the Software without restriction, including
+// without limitation the rights to use, copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, and to
+// permit persons to whom the Software is furnished to do so, subject to
+// the following conditions:
+// 
+// The above copyright notice and this permission notice shall be
+// included in all copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+// EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+// MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+// NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+// LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+// OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+// WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+
+using System;
+using System.IO;
+
+using MonoDevelop.Ide.Gui;
+using MonoDevelop.Ide.Gui.Content;
+using MonoDevelop.Projects.Gui.Completion;
+
+namespace MonoDevelop.RubyBinding
+{
+	public class RubyTextEditorExtension: CompletionTextEditorExtension
+	{
+		public override bool ExtendsEditor (Document doc, IEditableTextBuffer editor)
+		{
+			return (Path.GetExtension (doc.FileName).Equals (RubyLanguageBinding.RubyExtension, StringComparison.OrdinalIgnoreCase));
+		}
+		
+		public override ICompletionDataList HandleCodeCompletion (ICodeCompletionContext completionContext, char completionChar)
+		{
+			return null;
+		}
+		
+		public override ICompletionDataList CodeCompletionCommand (ICodeCompletionContext completionContext)
+		{
+			return null;
+		}
+		
+		public override  IParameterDataProvider HandleParameterCompletion (ICodeCompletionContext completionContext, char completionChar)
+		{
+			return null;
+		}
+	}
+}
